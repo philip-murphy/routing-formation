f9febd4895ddaf935bb0f0b93b70c553000d6293
roylines <roy@roylines.co.uk>
2015-02-24 12:06:07 +0100

    added deploy script for updating haproxy configuration

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/costacruise/routing-formation/master/haproxy.cfg -O /etc/haproxy/haproxy.cfg
+sed -i -- 's/ADMINUSER/AdminUser/g' /etc/haproxy/haproxy.cfg 
+sed -i -- 's/ADMINPASSWORD/AdminPassword/g' /etc/haproxy/haproxy.cfg 
+sed -i -- 's/S3BUCKET/S3Bucket/g' /etc/haproxy/haproxy.cfg 
+
+service haproxy restart
