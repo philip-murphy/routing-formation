90cef5924b58089b317776efa2373ebbad62f2cb
roylines <roy@roylines.co.uk>
2015-03-03 14:02:05 +0100

    allow configuration of s3 region

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ wget https://raw.githubusercontent.com/costacruise/routing-formation/master/hapr
 sed -i -- 's/ADMINUSER/AdminUser/g' /etc/haproxy/haproxy.cfg 
 sed -i -- 's/ADMINPASSWORD/AdminPassword/g' /etc/haproxy/haproxy.cfg 
 sed -i -- 's/S3BUCKET/S3Bucket/g' /etc/haproxy/haproxy.cfg 
+sed -i -- 's/S3REGION/S3Region/g' /etc/haproxy/haproxy.cfg 
 
 service haproxy restart
